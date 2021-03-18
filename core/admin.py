587aaf8c23ba81e3fc43844870b1ab7d9db7e989
Jose Carlos <jcnv.trabalho@gmail.com>
2021-03-18 15:42:32 -0300

    Criada a tabela de evento

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from core.models import Evento
 # Register your models here.
+
+class EventoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'data', 'datacriacao')
+    list_filter = ('titulo', 'datacriacao',)
+
+admin.site.register(Evento, EventoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data = models.DateTimeField(verbose_name='Data do Evento')
+    datacriacao = models.DateTimeField(auto_now=True, verbose_name='Data da Criação')
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "evento"
+
+    def __str__(self):
+        return self.titulo
